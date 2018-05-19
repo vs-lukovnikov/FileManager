011daa4ae97f041e1988dcb5e7184f8474ead676
Vsevolod Lukovnikov <vs.lukovnikoff@gmail.com>
2018-05-19 15:39:26 +0300

    Added FileManager

---
+++ FileManager.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * Trait TraitSingletone
+ */
+trait TraitSingletone {
+
+  private static $instance;
+
+  public static function getInstance($folder) {
+    if (self::$instance === NULL) {
+      self::$instance = new static($folder);
+    }
+    return self::$instance;
+  }
+}
+
+class FileManager {
+
+  use TraitSingletone;
+
+  /**
+   * @var array
+   */
+  public $files;
+
+  /**
+   * @var array contains criteries for sorting data.
+   */
+  public static $sort_criteria = [
+    'namedesc' => [
+      'type' => [SORT_ASC],
+      'name' => [SORT_DESC, SORT_NATURAL],
+    ],
+    'nameasc' => [
+      'type' => [SORT_ASC],
+      'name' => [SORT_ASC, SORT_NATURAL],
+    ],
+    'sizedesc' => [
+      'type' => [SORT_ASC],
+      'size' => [SORT_DESC, SORT_NATURAL],
+    ],
+    'sizeasc' => [
+      'type' => [SORT_ASC],
+      'size' => [SORT_ASC, SORT_NATURAL],
+    ],
+    'typedesc' => [
+      'type' => [SORT_DESC],
+    ],
+    'typeasc' => [
+      'type' => [SORT_ASC],
+    ],
+  ];
+
+  private function __construct($folder) {
+    $this->files = $this->getFiles($folder);
+  }
+
+  /**
+   * Create array with data of files and dirs contained in $directory.
+   *
+   * @param string $directory
+   *
+   * @return array $files array of files and dir's in input
+   * directory
+   */
+  public function getFiles($directory) {
+    $files = [];
+
+    if ($directory[strlen($directory) - 1] !== '/') {
+      $directory .= '/';
+    }
+
+    if ($catalog = opendir($directory)) {
+      while (FALSE !== ($item = readdir($catalog))) {
+        if (!in_array($item, [".", "..", ".git"])) {
+          $file = $directory . $item;
+          $file_name = pathinfo($item);
+          $file_size = filesize($file) / 8;
+          $file_type = filetype($file);
+          if ($file_type === 'dir') {
+            $path = $directory . $file_name['filename'];
+          }
+          $file_data = [
+            'name' => $file_name['filename'],
+            'extension' => $file_name['extension'] && $file_type == 'file' ? $file_name['extension'] : '',
+            'size' => $file_size,
+            'type' => $file_type,
+            'path' => $path,
+          ];
+          $files[] = $file_data;
+        }
+      }
+    }
+    closedir($catalog);
+    return $files;
+  }
+
+  /**
+   * Method sort $files array
+   *
+   * @param  array $data input array for sorting
+   * @param  array $sort_criteria sorting data criteries
+   * @param  bool $case_in_sensitive sort criteria - sensitive or not to
+   *   data register
+   *
+   * @return array  $files sorted array
+   */
+  public function sort($data, $sort_criteria, $case_in_sensitive = TRUE) {
+    if (!is_array($data) || !is_array($sort_criteria)) {
+      return FALSE;
+    }
+    $args = [];
+    $i = 0;
+
+    foreach($sort_criteria as $sort_column => $sort_attributes) {
+      $col_lists = [];
+      foreach ($data as $key => $row) {
+        $convert_to_lower = $case_in_sensitive && (in_array(SORT_STRING, $sort_attributes) || in_array(SORT_REGULAR, $sort_attributes));
+        $row_data = $convert_to_lower ? strtolower($row[$sort_column]) : $row[$sort_column];
+        $col_lists[$sort_column][$key] = $row_data;
+      }
+      $args[] = &$col_lists[$sort_column];
+
+      foreach ($sort_attributes as $sort_attribute) {
+        $tmp[$i] = $sort_attribute;
+        $args[] = &$tmp[$i];
+        $i++;
+      }
+    }
+    $args[] = &$data;
+    call_user_func_array('array_multisort', $args);
+    return end($args);
+  }
+}
---
+++ index.php
@@ -0,0 +1,89 @@
+<?php error_reporting(0); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <?php
+  require('FileManager.php');
+
+  isset($_GET['cat']) ? $cat = $_GET['cat'] : $cat = getcwd();
+  isset($_GET['sort']) ? $sort = $_GET['sort'] : $sort = 'name';
+  isset($_GET['order']) ? $order = $_GET['order'] : $order = 'asc';
+
+  $sort = $sort . $order;
+
+  $file_manager = FileManager::getInstance($cat);
+  $sort_criteria = $file_manager::$sort_criteria;
+  $files = $file_manager->sort($file_manager->files, $sort_criteria[$sort], TRUE);
+
+  isset($_GET['cat']) ? $path = $_GET['cat'] : $path = dirname(__FILE__);
+
+  $order = [
+    'name' => 'desc',
+    'type' => 'desc',
+    'size' => 'desc',
+  ];
+  ?>
+  <title>Test File Manager</title>
+</head>
+<body>
+<p>
+  <?php
+  echo "<p>Directory: " . $path . "</p>";
+
+  if ($path !== dirname(__FILE__)) {
+    $back = $_SERVER['PHP_SELF'] . "?cat=" . realpath($_GET['cat'] . '/../');
+    echo "<a href=" . $back . ">Exit</a>";
+  }
+  if (isset($_GET['sort'])) {
+    ($_GET['order'] === 'desc') ? $order[$_GET['sort']] = 'asc' : $order_name = 'desc';
+  }
+  ?>
+</p>
+<table>
+  <tr>
+    <th>
+      <a href="<?php echo $_SERVER['PHP_SELF'] . '?sort=name&order='
+        . $order['name']
+        . '&cat='
+        . realpath($cat); ?>">Name</a>
+    </th>
+    <th>
+      <a href="<?php echo $_SERVER['PHP_SELF'] . '?sort=type&order='
+        . $order['type']
+        . '&cat='
+        . realpath($cat); ?>">Type</a>
+    </th>
+    <th>
+      <a href="<?php echo $_SERVER['PHP_SELF'] . '?sort=size&order='
+        . $order['size']
+        . '&cat='
+        . realpath($cat); ?>">Size</a>
+    </th>
+  </tr>
+  <?php if ($files == NULL): ?>
+    <tr>
+      <td>No files</td>
+    </tr>
+  <?php else: ?>
+    <?php foreach ($files as $file): ?>
+      <tr>
+        <td>
+          <?php
+          if ($file['type'] === 'dir') {
+            $link = $_SERVER['PHP_SELF'] . "?cat=" . $path . '/' . $file['name'];
+            echo "<a href=" . $link . ">" . $file['name'] . "</a>";
+          }
+          else {
+            echo $file['name'] . '.' . $file['extension'];
+          }
+          ?>
+        </td>
+        <td><?php echo $file['type']; ?></td>
+        <td><?php echo $file['size'] . ' kB'; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  <?php endif; ?>
+</table>
+</body>
+</html>
